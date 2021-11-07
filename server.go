dc8ed348b0c9671ae7f3411e01ec0c6ac20a76c6
kai <347036700@qq.com>
2021-11-07 22:37:53 +0800

    v1

--- server.go
+++ server.go
@@ -1,29 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 type Server struct {
 	Ip   string
-	Prot int
+	Port int
 }
 
 //create a server
 func NewServer(ip string, port int) *Server {
-	server:=&Server{
-		Ip: ip,
-		Prot: port,
+	server := &Server{
+		Ip:   ip,
+		Port: port,
 	}
 	return server
 }
-
-//start server
-func (S *Server)Start()  {
-	//socket listen
-	listener,err :=  net.Listen()
-
-	//accept
-
-	//do handler
-
-	//close socket 
+func (s *Server) Handler(conn net.Conn) {
+	// current connect
+	fmt.Println("connect successfully")
 }
 
+//start server
+func (s *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("net.Listen err", err)
+	}
+	//close socket
+	defer listener.Close()
 
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		//do handler
+		go s.Handler(conn)
+	}
+}
